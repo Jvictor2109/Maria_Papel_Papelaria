c32f6ff93689e54d26cc8ba239457ab461ea32ee
Jvictor2109 <joaobrasil2109@gmail.com>
2026-05-07 23:47:16 +0100

    formulario de inserção funcionando e tabela sem botoes funcionando

--- assets/js/gestao_manual.js
+++ assets/js/gestao_manual.js
@@ -1,11 +1,37 @@
+// Carrega a tabela assim que carrega a pagina
+carregarTabela();
+
+
+// Validar para aceitar apenas números no ISBN
+document.getElementById('isbn').addEventListener('input', (e) => {
+    e.target.value = e.target.value.replace(/[^0-9]/g, '');
+});
+
 // Event listener pra quando digitar no campo ISBN
 document.getElementById('isbn').addEventListener('keyup', (e) =>{
     const form = document.querySelector('.form-add-manual');
+    const isbn = e.target.value.trim();
     if(e.target.value.length > 0 ){
         form.style.display = "flex";
+        form.querySelectorAll('input, select').forEach(el => el.disabled = false);
     }
     else{
         form.style.display = "none";
+    }
+
+    if(isbn.length == 10 || isbn.length == 13){
+        fetch('gestao_manual.php', {
+            method:"post",
+            headers:{ 'Content-Type': 'application/json' },
+            body:JSON.stringify({acao:"checar_isbn", isbn:isbn})
+        }).then(response =>response.json())
+        .then(data=>{
+            if(data["resultado"] == true){                
+                mostrarMsg("red", "ISBN já existente na base de dados");
+                // desativar os campos
+                form.querySelectorAll('input, select').forEach(el => el.disabled = true);
+            }
+        });
     }
 });
 
@@ -46,6 +72,7 @@ btn_guardar_manual.addEventListener('click', ()=>{
     })
 
     const dados = {
+        "acao":"adicionar",
         "isbn":isbn,
         "nome_manual":nome_manual,
         "cod_manual":cod_manual,
@@ -57,9 +84,113 @@ btn_guardar_manual.addEventListener('click', ()=>{
         "anos_escolares":ids_anos_escolares
     }
 
-    console.log(dados);
+    // Envia pro PHP
+    fetch('gestao_manual.php', {
+        method:"post",
+        headers:{ 'Content-Type': 'application/json' },
+        body:JSON.stringify(dados)
+
+    }).then(response =>response.json())
+    .then(data =>{
+        if(data["resultado"] == "sucesso"){
+            mostrarMsg("green", data["msg"]);
+        }
+        else if(data["resultado"] == "erro"){
+            mostrarMsg("red", data["msg"]);
+        }
+        
+    }); 
+
+    // Limpa os inputs e esconde o formulário após a adição
+    const textInputs = document.querySelectorAll('input[type="text"]');
+        textInputs.forEach(input => {
+            input.value = "";
+        });
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
     
+    const selects = document.querySelectorAll('select');
+        selects.forEach(select => {
+            select.selectedIndex = 0;
+        });
+
+    document.querySelector('.form-add-manual').style.display = "none";
 });
+
+
+// Função que popula a tabela
+function carregarTabela(){
+    let dados = {};
+
+    fetch('gestao_manual.php', {
+        method:"post",
+        headers:{ 'Content-Type': 'application/json' },
+        body:JSON.stringify({"acao":"listar_manuais"})
+    }).then(response => response.json())
+    .then(data =>{
+        dados = data;
+        console.log(dados);
+        
+        renderTabela(dados);
+    });
+
+    
+    
+}
+
+
+// Função que constroi a tabela
+function renderTabela(dados){
+    const tbody = document.querySelector('tbody');
+    tbody.innerHTML = '';
+
+    // Itera sobre cada linha e adiciona à tabela
+    dados.forEach(element =>{
+        let linha = document.createElement('tr');
+
+        let id_manual = document.createElement('td');
+        id_manual.innerText = element.id_manual;
+        linha.appendChild(id_manual);
+
+        let isbn_manual = document.createElement('td');
+        isbn_manual.innerText = element.isbn_manual;
+        linha.appendChild(isbn_manual);
+        
+        let nome_manual = document.createElement('td');
+        nome_manual.innerText = element.nome_manual;
+        linha.appendChild(nome_manual);
+        
+        let preco_manual = document.createElement('td');
+        preco_manual.innerText = element.preco_manual;
+        linha.appendChild(preco_manual);
+        
+        let cod_manual = document.createElement('td');
+        cod_manual.innerText = element.cod_manual;
+        linha.appendChild(cod_manual);
+        
+        let nome_editora = document.createElement('td');
+        nome_editora.innerText = element.nome_editora;
+        linha.appendChild(nome_editora);
+
+        // Vai ao servidor buscar todos os anos escolares associados ao livro
+        fetch('gestao_manual.php', {
+            method:"post",
+            headers:{ 'Content-Type': 'application/json' },
+            body:JSON.stringify({"acao":"checar_anos_escolares", "id_manual":element.id_manual})
+        }).then(response => response.json())
+        .then(data =>{
+            let anos_escolares = document.createElement('td');
+            anos_escolares.innerText = data.resultado.join('\n');
+            linha.appendChild(anos_escolares);            
+        });
+
+        tbody.appendChild(linha);
+    })
+}
+
 
 // Função mensagem de erro
 const msgErro = document.getElementById('msgErro');
@@ -71,3 +202,17 @@ function mostrarMsg(cor, conteudo) {
         msgErro.innerText = "";
     }, 2000)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- gestao_manual.php
+++ gestao_manual.php
@@ -1,9 +1,140 @@
 <?php
-	// Verificação de sessão em todas as páginas protegidas
-	session_start();
-	include('db_connect.php');
+// Verificação de sessão em todas as páginas protegidas
+session_start();
+include('db_connect.php');
 
-	?>
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	// Recebe a request do Javascript e lida conforme o tipo de ação
+	$request = json_decode(file_get_contents('php://input'), true);
+
+	switch($request["acao"]){
+		case "adicionar":
+			if(addManual($conn, $request)){
+			echo json_encode(['resultado' => 'sucesso', 'msg' => 'Manual adicionado com sucesso!']);
+			}
+			else{
+				echo json_encode(['resultado' => 'erro', 'msg' => 'Não foi possível adicionar à base de dados']);
+			};
+			exit();
+		
+		case "checar_isbn":
+			echo json_encode(['resultado' => checarIsbn($conn, $request)]);
+			exit();
+
+		case "listar_manuais":
+			echo json_encode(listarManuais($conn));
+			exit();
+
+		case "checar_anos_escolares":
+			echo json_encode(checarAnosEscolares($conn, $request));
+			exit();
+	}
+
+	
+}
+
+
+function checarAnosEscolares(mysqli $conn, array $request){
+	$id_manual = $request['id_manual'];
+
+	$sql = "SELECT nome_ano_escolar FROM ano_escolar 
+			JOIN manual_ano_escolar ON manual_ano_escolar.id_ano_escolar=ano_escolar.id_ano_escolar
+			WHERE id_manual = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("i", $id_manual);
+	$stmt->execute();
+	$resultado = $stmt->get_result();
+	$anos = [];
+	while($row = $resultado->fetch_assoc()){
+		$anos[] = $row['nome_ano_escolar'];
+	}
+	return ['resultado'=>$anos];
+}
+
+
+function listarManuais(mysqli $conn){
+	$sql = "SELECT * FROM manual JOIN editora ON manual.id_editora=editora.id_editora";
+	$resultado = mysqli_query($conn, $sql);
+	$dados = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+
+	return $dados;
+}
+
+
+function addManual(mysqli $conn, array $request){
+	// Extrai os dados da request
+	$isbn = $request["isbn"];
+	$nome_manual = $request["nome_manual"];
+	$cod_manual = $request["cod_manual"];
+	$preco_manual = floatval($request["preco_manual"]);
+	$editora = intval($request["editora"]);
+	$disciplina = intval($request["disciplina"]);
+	$tipo_manual = $request["tipo_manual"];
+	$agrupamentos = $request["agrupamentos"];
+	$anos_escolares = $request["anos_escolares"];
+
+	// Começa o processo de inserção de dados
+	// Ou tudo é inserido corretamente, ou nada é inserido
+	$conn->begin_transaction();
+
+	try{
+		// Insere o manual
+		$sql = "INSERT INTO manual (isbn_manual, nome_manual, preco_manual, cod_manual, id_editora, id_disciplina, tipo_manual)
+		VALUES (?,?,?,?,?,?,?)";
+
+		$stmt = $conn->prepare($sql);
+		$stmt->bind_param("ssdsiis", $isbn,$nome_manual,$preco_manual,$cod_manual, $editora,$disciplina,$tipo_manual);
+		$stmt->execute();
+		$id_manual = $conn->insert_id;
+
+		$stmt->close();
+
+		// Insere as relações de agrupamento
+		$sql = "INSERT INTO manual_agrupamento (id_manual, id_agrupamento) VALUES (?,?)";
+		$stmt = $conn->prepare($sql);
+		foreach($agrupamentos as $id_agrupamento){
+			$stmt->bind_param("ii", $id_manual, $id_agrupamento);
+			$stmt->execute();
+
+		}
+		$stmt->close();
+		
+		// Insere as relações de anos escolares
+		$sql = "INSERT INTO manual_ano_escolar (id_manual, id_ano_escolar) VALUES (?,?)";
+		$stmt = $conn->prepare($sql);
+		foreach($anos_escolares as $id_ano_escolar){
+			$stmt->bind_param("ii", $id_manual, $id_ano_escolar);
+			$stmt->execute();
+		}
+		$stmt->close();
+
+		$conn->commit();
+		return true;
+
+	}catch(Exception $e){
+		$conn->rollback();
+		return false;
+	}	
+}
+
+
+function checarIsbn(mysqli $conn, array $request){
+	$isbn = $request["isbn"];
+
+	$sql = "SELECT * FROM manual WHERE isbn_manual = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("s", $isbn);
+	$stmt->execute();
+	$resultado = $stmt->get_result();
+	if($resultado->num_rows > 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+?>
 
 	<!DOCTYPE HTML>
 	<!--
@@ -41,7 +172,7 @@
 													<div class="col-8">
 														<!-- Campo ISBN para começar a adicionar um manual -->
 														<label for="isbn">ISBN:</label>
-														<input style="width: 40%; margin-bottom: 10px;" type="text" id="isbn" name="isbn">
+														<input pattern="[0-9]*" style="width: 40%; margin-bottom: 10px;" type="text" id="isbn" name="isbn" minlength="10" maxlength="13">
 
 														<!-- Restantes campos só aparecem ao digitar algo no ISBN -->
 														 <div class="form-add-manual row">
@@ -150,6 +281,31 @@
 
 														 </div>
 														
+													</div>
+												</div>
+
+												<div class="row">
+													<div class="col-12">
+														<!-- Tabela que mostra os itens -->
+														<div class="table-wrapper" style="max-height: 400px; overflow-y: auto;">
+															<table>
+																<thead>
+																	<tr>
+																		<th>ID</th>
+																		<th>ISBN</th>
+																		<th>Nome</th>
+																		<th>Preço</th>
+																		<th>Código</th>
+																		<th>Editora</th>
+																		<th>Ano Escolar</th>
+																		<th>   </th>
+																	</tr>
+																</thead>
+																<tbody>
+
+																</tbody>
+															</table>
+														</div>
 													</div>
 												</div>
 												
